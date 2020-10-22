20f8439986b8fe1f8bae48ba24f10175ae7d908b
Ryo.Makabe <ryo0928dance@gmail.com>
2020-10-22 07:56:45 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
      @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def new
-     @user = User.new
+    @user = User.new
   end
 
   def create
